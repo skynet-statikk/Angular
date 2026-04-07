3980bdcd06a0805c5653ea99578f0529ea38695d
Michael Trullas <adoser2@hotmail.es>
2026-04-07 02:13:23 +0200

    fix msw

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { isDevMode } from '@angular/core';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { worker } from './testing/handlers';
 
 async function bootstrap() {
+  if (isDevMode()) {
+    await worker.start();
+  }
+
   await bootstrapApplication(App, appConfig);
 }
 
